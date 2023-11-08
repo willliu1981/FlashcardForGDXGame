54af8f2be095e1067d4f5e62a9ba9c284410f7e4
willliu1981 <h111107231675>
2023-11-08 23:52:05 +0800

    測試libgdx ui:button & text

--- core/src/idv/kuan/flashcard/gdx/game/TestScreen.java
+++ core/src/idv/kuan/flashcard/gdx/game/TestScreen.java
@@ -5,13 +5,21 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 import java.sql.Connection;
 import java.util.List;
+
+import javax.swing.JTextField;
 
 import idv.kuan.libs.databases.schema.modifier.DatabaseSchemaUtils;
 import idv.kuan.libs.databases.schema.modifier.SchemaModifier;
@@ -28,6 +36,8 @@ public class TestScreen implements Screen {
     SpriteBatch batch;
     Texture img;
 
+    TextField textField;
+
     public TestScreen(VersionHelper versionHelper) {
         batch = new SpriteBatch();
         img = new Texture("badlogic.jpg");
@@ -36,9 +46,31 @@ public class TestScreen implements Screen {
 
         checkSchema();
 
-        viewport = new StretchViewport(800, 100);
+        viewport = new StretchViewport(800, 400);
         stage = new Stage(viewport);
         Gdx.input.setInputProcessor(stage);
+
+
+        Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
+
+        TextButton button = new TextButton("click me", skin);
+        button.setPosition(150, 200);
+        button.setSize(200, 50);
+        button.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                System.out.println("xxx TS:Button pressed");
+                ((TextButton) actor).setText(textField.getText());
+            }
+        });
+
+
+        textField = new TextField("edit...", skin);
+        textField.setPosition(150, 300);
+        textField.setSize(200, 50);
+
+        stage.addActor(button);
+        stage.addActor(textField);
     }
 
     private void checkSchema() {
@@ -49,30 +81,18 @@ public class TestScreen implements Screen {
             public void execute(SchemaModifierHandler.SchemaModifierBuilder modifierBuilder, List<SchemaModifier> modifiers) {
                 TableSchemaModifier word = modifierBuilder.setConstructionSql("CREATE TABLE \"word\" ( " +
                         " \"id\" INTEGER NOT NULL UNIQUE, " +
-                        " \"term_1\" TEXT NOT NULL, " +
-                        " \"translation_1y\" TEXT NOT NULL, " +
+                        " \"term\" TEXT NOT NULL, " +
+                        " \"translation\" TEXT NOT NULL, " +
                         " \"at_created\" TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
                         " \"at_updated\" TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
                         " \"metadata\" BLOB, " +
                         " PRIMARY KEY(\"id\" AUTOINCREMENT) " +
                         ")").setTableName("word").createSchemaModifier(TableSchemaModifier.class);
-                word.setNewColumns("id,term_1,translation_1y,at_created,at_updated,metadata");
-                word.setOldColumns("id,term_1,translation_1x,at_created,at_updated,metadata");
-
-                TableSchemaModifier word2 = modifierBuilder.setConstructionSql("CREATE TABLE \"word2\" ( " +
-                        " \"id\" INTEGER NOT NULL UNIQUE, " +
-                        " \"term_2\" TEXT NOT NULL, " +
-                        " \"translation_2y\" TEXT NOT NULL, " +
-                        " \"at_created\" TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
-                        " \"at_updated\" TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
-                        " \"metadata\" BLOB, " +
-                        " PRIMARY KEY(\"id\" AUTOINCREMENT) " +
-                        ")").setTableName("word2").createSchemaModifier(TableSchemaModifier.class);
-                word2.setNewColumns("id,term_2,translation_2y,at_created,at_updated,metadata");
-                word2.setOldColumns("id,term_2,translation_2x,at_created,at_updated,metadata");
+                word.setNewColumns("id,term,translation,at_created,at_updated,metadata");
+                word.setOldColumns("id,term,translation,at_created,at_updated,metadata");
 
                 modifiers.add(word);
-                modifiers.add(word2);
+
             }
         });
     }
@@ -86,14 +106,16 @@ public class TestScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        stage.act(Gdx.graphics.getDeltaTime());
+
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
         stage.draw();
 
-        ScreenUtils.clear(1, 1, 1, 1f);
-        batch.begin();
-        batch.draw(img, 0, 0);
-        batch.end();
+        //ScreenUtils.clear(1, 1, 1, 1f);
+        //batch.begin();
+        //batch.draw(img, 0, 0);
+        //batch.end();
 
     }
 
