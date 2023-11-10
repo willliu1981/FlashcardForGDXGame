b8c46b6f1989ecc9e52b58ad63f4248e93f00f3d
willliu1981 <h111107231675>
2023-11-10 19:40:48 +0800

    測試desktop app

---
+++ desktop/src/idv/kuan/flashcard/gdx/game/DesktopVersionHelper.java
@@ -0,0 +1,14 @@
+
+        package idv.kuan.flashcard.gdx.game;
+        import idv.kuan.libs.impl.DefaultVersionHelper;
+
+        public class DesktopVersionHelper extends DefaultVersionHelper{
+                public String getVersionName() {
+                    return "1.0";
+                }
+
+                public int getVersionCode() {
+                    return 8;
+                }
+        }
+        
